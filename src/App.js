df1f8f9a214063e5480fc160b6194bcedaa083e8
robert-robinson <robert.robinson.gis@gmail.com>
2022-06-02 23:16:15 +0100

    version control

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import LILogo from './LI-Logo.png';
 import './App.css';
 
 function App() {
@@ -10,13 +11,14 @@ function App() {
           
           Robert Robinson Presents...
         </p>
+        <img src={LIlogo} className="Linkedin-logo" alt="Linkedin" />
         <a
           className="App-link"
           href=""
-          target="_blank"
+          target="https://www.linkedin.com/in/robert-robinson-0846a5170/"
           rel="noopener noreferrer"
         >
-          LinkedIn
+          Linkedin
         </a>
       </header>
     </div>
